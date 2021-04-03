05dd4a17c038454ca41e9af7f3d4a137f1d498ae
Driele An <drielean@gmail.com>
2021-04-03 13:21:13 -0300

    done

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,89 +1,138 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 
-import LoadGif from '../../img/giphy.gif';
-import { HomeContainer } from './style';
-import api from '../../service/api';
+import api from '../../service/api'
+import { jokeByCategoryType, IJoke } from '../../utils/types'
+import ChuckGif from '../../img/giphy.gif'
+import { HomeContainer } from './style'
 
-
-interface IJoke{
-    id: string;
-    icon_url: string;
-    value: string;
+const emptySearchResults = {
+  id: '',
+  icon_url: '',
+  value: ''
 }
 
 const Home: React.FC = () => {
-    const [ categoriesJoke, setCategoriesJoke ] = useState([]);
-    const [ categorySelected, setCategorySelected ] = useState<IJoke>()
-    const [ searchJoke, setSearchJoke ] = useState('')
-    const [ isLoad, setIsLoad ] = useState(false)
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const [ categoriesJoke, setCategoriesJoke ] = useState([] as any)
+    const [ categorySelected, setCategorySelected ] = useState<jokeByCategoryType | null>()
+    const [ searchJoke, setSearchJoke ] = useState<string>('')
+    const [ searchField, setSearchField ] = useState<string>('')
+    const [ isLoading, setIsLoading ] = useState<boolean>(false)
     const [ resultSearch, setResultSearch ] = useState<IJoke[]>([])
+    const [ activeButton, setActiveButton ] = useState<string>('')
 
+    // useEffect( () => {
+    //     api.get('jokes/categories').then(
+    //         response => {
+    //             setCategoriesJoke(response.data)
+    //         }
+    //     )
+    // }, [])
 
-    useEffect( () => {
-        api.get('jokes/categories').then(
-            response => {
-                setCategoriesJoke(response.data)
-            }
-        )
-    }, [])
-
-    async function handleJokes(){
-        setIsLoad(true)
-        const response = await api.get(`jokes/search?query=${searchJoke}`)
-        setResultSearch(response.data.result)
-        if(response.status === 200 ){
-            setIsLoad(false)
-        } 
+    const getCategories = async () => {
+      try {
+        setIsLoading(true)
+        const response = await api.getJokeCategories()
+        setCategoriesJoke(response)
+      } catch (error) {
+        console.log(error)
+      } finally {
+        setIsLoading(false)
+      }
     }
 
-    async function handleJokeByCategory(e: string){
-        setIsLoad(true)
-        const response = await api.get(`jokes/random?category=${e}`)
-        setCategorySelected(response.data)
-        if( response.status === 200 ){ 
-            setIsLoad(false)
-        } 
-
-
-
+    const hook = () => {
+      getCategories()
     }
 
+    useEffect(hook, [])
+
+    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setSearchField(e.target.value)
+      setSearchJoke(e.target.value)
+    }
+
+    const handleJokes = async () => {
+      try {
+        setIsLoading(true)
+        const response = await api.searchJoke(searchJoke)
+        setResultSearch(response.result)
+        setCategorySelected(null)
+        setActiveButton('')
+      } catch (error) {
+        console.log(error)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    const handleJokeByCategory = async (term: string) => {
+      try {
+        setIsLoading(true)
+        setSearchJoke('')
+        setSearchField('')
+        setActiveButton(term)
+
+        const response = await api.getJokeByCategory(term)
+        setCategorySelected(response)
+      } catch (error) {
+        console.log(error)
+      } finally {
+        setResultSearch([emptySearchResults])
+        setIsLoading(false)
+      }
+    }
 
     return (
         <HomeContainer>
+          <div className="home-content">
+          <h1 style={{textAlign: 'center'}}>Norris Jokes</h1>
+          <div className="search-box">
+            <h2>Find Joke</h2>
+            <div className="input-group">
+              <input type="text" onChange={handleSearch} value={searchField}/>
+              <button type="submit" onClick={handleJokes} disabled={searchField?.length ? false : true}> Find joke</button>
+            </div>
+          </div>
+
             <div className="categories">
                 <ul>
-                { categoriesJoke.map( (joke, index) => (
-                    <li key={index} onClick={ () => handleJokeByCategory(joke) }>{joke}</li>
-                ))}
+                  {
+                    categoriesJoke.map((joke: string, index: number) => (
+                      <li
+                        className={activeButton === joke ? "active category" : "category"}
+                        key={index}
+                        onClick={() => handleJokeByCategory(joke) }>{joke}</li>
+                    ))
+                  }
                 </ul>
             </div>
-            <div>
-                <img src={categorySelected?.icon_url} alt={categorySelected?.value}/>
-                <h3>{categorySelected?.value}</h3>
+
+            <div className={ categorySelected ? "joke-card" : "" }>
+              <img src={categorySelected?.icon_url} alt={categorySelected?.value}/>
+              <h3>{categorySelected?.value}</h3>
             </div>
 
-            <h2>Find Joke</h2>
+            { resultSearch && resultSearch[0] && resultSearch[0].id
+              ? <h3>Results found for: {searchJoke}</h3>
+              : ''
+            }
 
-            <div className="input-group">
-                <input type="text" onChange={ e => setSearchJoke(e.target.value)} />
-                <button type="submit" onClick={handleJokes}> Find joke</button>
+            <div className={ isLoading ? "search-gif" : "joker"}>
+              {
+                isLoading
+                ? <img className="search-gif" src={ChuckGif} alt="load"/>
+                : resultSearch.map( result => (
+                  <div className={ result && result.value ? "joke-card" : "" } key={result.id}>
+                      <img src={result.icon_url} alt={result.value}/>
+                      <h3>{result.value}</h3>
+                  </div>
+                ))
+              }
             </div>
-            <p>Find results for:</p>
-            <div className="joker">
-                { isLoad ? <img src={LoadGif} alt="load"/> : resultSearch.map( result => (
-                    <div key={result.id}>
-                        <img src={result.icon_url} alt={result.value}/>
-                        <h3>{result.value}</h3>
-                    </div>
-                )) }
-
-            </div>
-            
-            
-
+          </div>
         </HomeContainer>
-    );
+    )
 }
 
-export default Home;
+export default Home
--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -1,19 +1,93 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 export const HomeContainer = styled.div`
-    width: 80%;
+  padding-bottom: 2rem;
+  min-height: 100vh;
+  width: 100%;
+  background: linear-gradient(347deg, rgba(144,140,212,1) 0%, rgba(127,255,212,1) 100%);
+  .home-content {
     margin: auto;
-
-    .categories{
-        display: flex;
+    width: 70%;
+    .search-box {
+      padding: 1rem;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      align-items: center;
+      h2 {
+        margin: 0.75rem;
+      }
     }
-
-    .input-group{
-
+  }
+  .categories {
+    margin: 1rem;
+    display: flex;
+    justify-content: center;
+    ul {
+      width: 70%;
+      list-style: none;
+      flex-wrap: wrap;
+      display: flex;
     }
-
-    .joker{
-        display: block;
+  }
+  .category {
+    margin: 0.5rem;
+    padding: 0.3rem;
+    padding-top: 0.5rem;
+    vertical-align: bottom;
+    min-width: 6rem;
+    height: 2rem;
+    background: whitesmoke;
+    color: #777;
+    border-radius: 0.2rem;
+    box-shadow: 2px 4px 5px 1px rgba(0,0,0,0.75);
+    transition: all 0.5s;
+    &:hover {
+      cursor: pointer;
+      color: #222;
+      box-shadow: 1px 3px 3px 0px rgba(0,0,0,0.75);
     }
-
+  }
+  .active {
+    color: #222;
+    box-shadow: 1px 3px 3px 0px rgba(0,0,0,0.75);
+  }
+  .input-group {
+    input[type=text] {
+      height: 2rem;
+      font-family: 'Chango', cursive;
+      padding: 0.25rem;
+      font-size: 1rem;
+    }
+    button[type=submit] {
+      box-shadow: 1px 3px 3px 0px rgba(0,0,0,0.75);
+      border-radius: 1rem;
+      margin-left: 0.5rem;
+      padding: 0.5rem;
+      font-family: 'Chango', cursive;
+      &:hover {
+        cursor: pointer;
+        background: yellowgreen;
+      }
+    }
+  }
+  .joke-card {
+    background: whitesmoke;
+    display: flex;
+    margin: 1rem 0;
+    padding: 0.5rem;
+    align-items: center;
+    box-shadow: 1px 2px 2px rgba(0,0,0,0.75);
+    border-radius: 0.2rem;
+    img {
+      padding-right: 1rem;
+    }
+  }
+  .joker {
+    display: block;
+  }
+  .search-gif {
+    margin: 1rem auto;
+    text-align: center;
+  }
 `
